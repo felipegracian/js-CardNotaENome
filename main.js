9c02dc132a01ae5d7c7d71fa2a33aeb69b738b7c
Felipe Graciano <22282790@cfp127aluno.local>
2023-02-16 16:44:36 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const adicionar = document.getElementById('adicionar')
+
+//const adicionarCard = () => {
+//  const container = document.getElementById('container')
+//container.innerHTML = container.innerHTML + '<div class = "item"></div>'
+//}
+
+const adicionarCard = (nome, nota) => {
+    const container = document.getElementById('container')
+    const novaDiv = document.createElement('div')
+    if (nome == '') {
+        alert('ERRO: Por favor digite o nome')
+    } else if (nota == '') {
+        alert('ERRO: Por favor digite o nome')
+    } else if (isNaN(nome) == false) {
+        alert('ERRO: Por favor digite não digite números no nome')
+    } else if (isNaN(nota) == true) {
+        alert('ERRO: Por favor digite não digite letras na nota')
+    } else if(nota < 0 || nota > 10){
+        alert('ERRO: As notas variam apenas entre 0 a 10')
+    } else {
+        if(nota > 5){
+            novaDiv.classList.add('aluno__verde')
+        } else if(nota < 6){
+            novaDiv.classList.add('aluno__vermelho')
+        } 
+        novaDiv.innerHTML = `<h2 class = "aluno__nome">${nome}</h2>
+        <span class = "aluno__nota">${nota}</span>`
+        container.appendChild(novaDiv)
+    }
+
+
+
+}
+
+const handleClick = () => {
+    const nome = prompt('Digite o nome do Aluno')
+    const nota = prompt('Digite a nota desse Aluno')
+    adicionarCard(nome, nota)
+
+}
+adicionar.addEventListener('click', handleClick)
